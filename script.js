655f572993166e39525713cf671ca6b7f4ae4aca
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-18 22:44:25 +0600

    coded added

---
+++ script.js
@@ -0,0 +1,76 @@
+document.getElementById('ranndomNumberGenerate').addEventListener('click',function(){
+    getRandomNumber(); 
+})
+
+function  getPin (){
+    const pin= Math.floor(Math.random()*10000);
+    const pinString = pin+"";
+
+    if(pinString.length=='4'){
+        document.getElementById('randomNumberDisplay').value= pin; 
+    }
+    else{
+        getPin(); 
+    }
+}
+
+function getRandomNumber(){
+    return getPin(); 
+}
+
+
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const previousNumber = document.getElementById('displayTypedNumber'); 
+   
+    const typedNumber = event.target.innerText; 
+    if(!isNaN(typedNumber)){
+        const newNumber = previousNumber.value+typedNumber; 
+        previousNumber.value = newNumber; 
+    }
+    if(isNaN(typedNumber)){
+        if(typedNumber=='C'){
+            previousNumber.value= "";
+        }
+    }
+})
+
+    let buttonClicked = 0;
+function countTrace(){
+    const countPlace= document.getElementById('count'); 
+    let countValue = countPlace.innerText;
+    buttonClicked++; 
+    if(buttonClicked==1){
+        countValue--; 
+        countPlace.innerText= countValue; 
+    }
+    else if(buttonClicked==2){
+        countValue--; 
+        countPlace.innerText= countValue; 
+    }
+    else if(buttonClicked==3){
+        countValue--; 
+        countPlace.innerText= countValue; 
+    }
+    if(countValue==0){
+        countPlace.innerText= 3;
+    }
+}
+ 
+
+document.getElementById('submitBtn').addEventListener('click',function(){
+    countTrace(); 
+    const randomPin = document.getElementById('randomNumberDisplay').value;
+    
+    const succcss= document.getElementById('successNumber'); 
+    const error = document.getElementById('errorNumber'); 
+    const typed = document.getElementById('displayTypedNumber').value;
+ 
+    
+    if(typed==randomPin){
+        succcss.style.display="block";         
+    }
+    if(typed!=randomPin){
+        error.style.display="block"; 
+    }
+})
